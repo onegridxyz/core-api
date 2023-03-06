89da30949ff121ad1fa353b1c684c8d95da198e6
Truong Le <travistrle@gmail.com>
2023-03-05 21:21:10 -0600

    OG-2 init commit

---
+++ src/main/kotlin/xyz/onegrid/coreapi/config/DatabaseConfiguration.kt
@@ -0,0 +1,13 @@
+
+package xyz.onegrid.coreapi.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.transaction.annotation.EnableTransactionManagement
+
+@Configuration
+@EnableJpaRepositories("xyz.onegrid.coreapi.repository")
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableTransactionManagement
+class DatabaseConfiguration
