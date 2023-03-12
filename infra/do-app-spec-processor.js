7ac1b070e49f60576041cd925f4911064b3292b1
Trường Lê <travistrle@gmail.com>
2023-03-11 20:50:49 -0600

    OG-2 load app spec (#16)

---
+++ infra/do-app-spec-processor.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('infra/do-app-spec-input.json');
+let appSpecObj = JSON.parse(rawdata);
+appSpecObj['services'][0]['image']['tag'] = process.env.IMAGE_TAG
+
+fs.writeFile ("infra/do-app-spec.json", JSON.stringify(appSpecObj), function(err) {
+    if (err) throw err;
+    console.log('complete');
+  }
+);
