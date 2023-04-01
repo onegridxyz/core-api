3f6c2389640a1f661abea30bb252d16c2d63e55b
Trường Lê <travistrle@gmail.com>
2023-04-01 11:10:55 -0500

    OG-32 add jhipster (#1)

---
+++ src/main/kotlin/xyz/onegrid/coreapi/web/rest/errors/EmailAlreadyUsedException.kt
@@ -0,0 +1,9 @@
+package xyz.onegrid.coreapi.web.rest.errors
+
+class EmailAlreadyUsedException :
+    BadRequestAlertException(EMAIL_ALREADY_USED_TYPE, "Email is already in use!", "userManagement", "emailexists") {
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+}
