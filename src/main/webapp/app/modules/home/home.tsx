091d3da646257cc206bcb1a68b14fab86a87acf2
Trường Lê <travistrle@gmail.com>
2023-04-09 10:05:56 -0500

    OG-32 remove default admin & user (#6)

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -25,17 +25,6 @@ export const Home = () => {
         ) : (
           <div>
             <Alert color="warning">
-              If you want to
-              <span>&nbsp;</span>
-              <Link to="/login" className="alert-link">
-                sign in
-              </Link>
-              , you can try the default accounts:
-              <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;) <br />- User (login=&quot;user&quot; and
-              password=&quot;user&quot;).
-            </Alert>
-
-            <Alert color="warning">
               You don&apos;t have an account yet?&nbsp;
               <Link to="/account/register" className="alert-link">
                 Register a new account
