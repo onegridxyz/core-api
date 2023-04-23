30fca2ce60b38b02d1371cba8c773351f3467c9b
Truong Le <travistrle@gmail.com>
2023-04-23 10:53:04 -0500

    OG-32 add Category controller

--- src/main/java/xyz/onegrid/coreapi/web/rest/publicapi/controller/CategoryController.java
+++ src/main/java/xyz/onegrid/coreapi/web/rest/publicapi/controller/CategoryController.java
@@ -2,10 +2,18 @@ package xyz.onegrid.coreapi.web.rest.publicapi.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import xyz.onegrid.coreapi.web.rest.publicapi.api.Category;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
 
     private static final Map<Long, Category> categoryDataStore = new HashMap<>();
@@ -17,24 +25,29 @@ public class CategoryController {
         categoryDataStore.put(4L, new Category(4L, "Category 4", 40));
     }
 
-    public Category getCategory(Long id) {
+    @GetMapping("/categories/{id}")
+    public Category getCategory(@PathVariable Long id) {
         return categoryDataStore.get(id);
     }
 
-    public Category createCategory(Category category) {
+    @PostMapping("/categories")
+    public Category createCategory(@RequestBody Category category) {
         categoryDataStore.put(category.getId(), category);
         return category;
     }
 
-    public Category updateCategory(Category category) {
+    @PutMapping("/categories")
+    public Category updateCategory(@RequestBody Category category) {
         categoryDataStore.put(category.getId(), category);
         return category;
     }
 
-    public Category deleteCategory(Long id) {
+    @DeleteMapping("/categories/{id}")
+    public Category deleteCategory(@PathVariable Long id) {
         return categoryDataStore.remove(id);
     }
 
+    @GetMapping("/categories")
     public Map<Long, Category> getAllCategories() {
         return categoryDataStore;
     }
